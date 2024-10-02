2f17bb4ce95d2b792d82913ad8579e1742fe7405
Dren <drenuruqi95@gmail.com>
2024-10-02 12:37:42 +0200

    added AspectVGrid which resizes the cards based on how many cards are there

---
+++ Memorize/View/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Dren Uruqi on 30.9.24.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
